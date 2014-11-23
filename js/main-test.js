38c17b91324f9e8ade529494e8fdb9dcb068b7ea
Kyle Prestenback <kyle.prestenback@disney.com>
2014-11-22 18:48:07 -0800

    te

--- js/main-test.js
+++ js/main-test.js
@@ -6,5 +6,20 @@ $.scrollTo('#events',{
 					duration: 1000,
 					offset: { 'left':0, 'top':-0.12*$(window).height() }
 				});
+});
+$(function() {
+
+$("#nav ul li a").click( function(event) {
+
+console.log("scrolled click 009");
+	console.log($(this).attr("href")); 
+	$.scrollTo($(this).attr("href"),{
+					duration: 1000,
+					offset: { 'left':0, 'top':-0.12*$(window).height() }
+				});
+	console.log("scrolled  009");
+	//event.preventDefault();
+
+});
 
 });
