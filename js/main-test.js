0a5ef187031c7fac1c4fa73e28209179073ee513
Kyle Prestenback <kyle.prestenback@disney.com>
2014-11-22 19:27:35 -0800

    re

--- js/main-test.js
+++ js/main-test.js
@@ -49,3 +49,35 @@ console.log("scrolled click 010");
 
 });
 */
+
+(function(){
+    var THROTTLE = 100,//ms
+        _timer = 0,
+        _dom = document.documentElement,
+        _width = _dom.style.width,
+        reset = function(){
+            // reset size, unfortunately forces another reflow
+            _dom.style.width = _width;
+        },
+        forceReflow = function(){
+            if (_timer) {
+                clearTimeout(_timer);
+                _timer = 0;
+            }
+ 
+            _width = _dom.style.width;
+ 
+            // force a reflow by increasing size 1px
+            _dom.style.width = (_dom.offsetWidth+1)+'px';
+ 
+            setTimeout(reset, 0);
+        },
+        onscroll = function() {
+            if (_timer) {
+                clearTimeout(_timer);
+            }
+            _timer = setTimeout(forceReflow, THROTTLE);
+        };
+ 
+    window.addEventListener('scroll', onscroll, false);
+})();
