6e1798c01eecd339106d3d57cd2f5e160147df0f
Kyle Prestenback <kyle.prestenback@disney.com>
2014-11-22 19:00:01 -0800

    w

--- js/main-test.js
+++ js/main-test.js
@@ -1,24 +1,28 @@
 
 $(function() {
-console.log("scrolled 009");
-
+console.log("scrolled 010");
+/*
 $.scrollTo('#events',{
 					duration: 1000,
-					offset: { 'left':0, 'top':-0.12*$(window).height() }
+					offset: { 'left':0, 'top':-0.12*$(window).height() },
+
 				});
+*/
+$.scrollTo( '#events', 1000, {axis:'y'} );
+
 });
 $(function() {
 
 $("#nav ul li a").click( function(event) {
 
-console.log("scrolled click 009");
+console.log("scrolled click 010");
 	console.log($(this).attr("href")); 
 	$.scrollTo($(this).attr("href"),{
 					duration: 1000,
-					offset: { 'left':0, 'top':-0.12*$(window).height() }
-				});
-	console.log("scrolled  009");
-	//event.preventDefault();
+					//offset: { 'left':0, 'top':-0.12*$(window).height() },
+				},{axis:'y'});
+	console.log("scrolled  010");
+	event.preventDefault();
 
 });
 
